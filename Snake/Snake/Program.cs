764092a16c3b0d83e689776eea3d6b5d2b59ef42
exit-devel <exit8523@gmail.com>
2016-04-28 17:13:35 +0300

    Создан класс Figure, родительский для HorizontalLine и VerticalLine

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,19 +10,21 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetBufferSize(80, 25);
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            //Отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
+            upLine.Draw();
+            downLine.Draw();
+            leftLine.Draw();
+            rightLine.Draw();
 
-            HorizontalLine hLine = new HorizontalLine(5, 10, 8, '+');
-            hLine.Draw();
-
-            VerticalLine vLine = new VerticalLine(5, 9, 15, '#');
-            vLine.Draw();
-
-            Console.ReadLine();
+            //Отрисовка точек
+            Point p = new Point(4, 5, '*');
+            p.Draw();
         }
     }
 }
--- Snake/Snake/VerticalLine.cs
+++ Snake/Snake/VerticalLine.cs
@@ -6,11 +6,9 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class VerticalLine
+    class VerticalLine : Figure
     {
-        List<Point> pList;
-
-        public VerticalLine(int x, int yUp, int yDown, char sym)
+        public VerticalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
             for (int y = yUp; y <= yDown; y++)
@@ -19,14 +17,6 @@ namespace Snake
                 pList.Add(p);
             }
 
-        }
-
-        public void Draw()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
         }
     }
 }
