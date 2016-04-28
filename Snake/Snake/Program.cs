c484fa69d9f855df6c19050a0215d386f9a310f2
exit-devel <exit8523@gmail.com>
2016-04-28 12:42:39 +0300

    Изучен класс List

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -16,6 +16,24 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
+            Point p3 = new Point(6, 7, '&');
+            p3.Draw();
+
+            Point p4 = new Point(8, 7, '%');
+            p4.Draw();
+
+            
+            
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            /*foreach (Point p in pList)
+            {
+                pList[p].Draw();
+            }*/
+
             Console.ReadLine();
         }
     }
