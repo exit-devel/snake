bb4ceb491219be73b981bd7049479bd726ff2324
exit-devel <exit8523@gmail.com>
2016-05-12 12:54:52 +0300

    Создан класс FoodCreator, учим змейку кушать и расти

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -27,16 +27,28 @@ namespace Snake
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
             
             while(true)
             {
-                if(Console.KeyAvailable)
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                } else {
+                    snake.Move();
+                }
+
+                Thread.Sleep(150);
+
+                if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(100);
-                snake.Move();
             }
         }
     }
